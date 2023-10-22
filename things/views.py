78a7a1f0dc96852a24795731403a3f2907c1191b
Sanchusri Kavitha Babu <sanchu2804kb@gmail.com>
2023-10-22 19:25:10 +0100

    Add Things form

--- things/views.py
+++ things/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import ThingsForm
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    return render(request, 'home.html')
+
+def thing(request):
+    form = ThingsForm()
+    return render(request, 'thing.html', {'form': form})
